1cfa06b4c0b7c1a9089a5e694ebf886e1b53d64d
Greg Schwartz <greglacrossse@gmail.com>
2022-03-15 20:35:29 -0400

    Double Collector Power

--- src/main/cpp/commands/Collect.cpp
+++ src/main/cpp/commands/Collect.cpp
@@ -21,7 +21,7 @@ void Collect::Initialize() {
 
 // Called repeatedly when this Command is scheduled to run
 void Collect::Execute() {
-  m_collector->SetMotorPO((m_elevator->GetSwitch1() || m_elevator->GetSwitch2())?-0.5:0);
+  m_collector->SetMotorPO((m_elevator->GetSwitch1() || m_elevator->GetSwitch2())?-1:0);
   m_elevator->SetMotorPO(m_elevator->GetSwitch1()?0.5:0);
 }
 
