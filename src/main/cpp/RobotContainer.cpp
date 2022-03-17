4412efce77cb1c773a9702d7247bcf69a2b29d56
Greg Schwartz <greglacrossse@gmail.com>
2022-03-16 22:20:32 -0400

    Finish Remapping Controls
    Missed the functions.

--- src/main/cpp/RobotContainer.cpp
+++ src/main/cpp/RobotContainer.cpp
@@ -69,8 +69,8 @@ void RobotContainer::ConfigureButtonBindings() {
     m_manRT.WhileHeld(Shoot(&m_shooter, &m_elevator, &m_collector, robotConfig["shootingSpeedFront"], robotConfig["shootingSpeedBack"]));
     m_manA.WhileHeld(Collect(&m_collector, &m_elevator));
     m_manB.WhileHeld(m_Uncollect);
-    m_manX.WhileHeld(m_CollectorSwap);
-    //m_manDPadRight.WhenPressed(Center(&m_driveBase, &m_shooter));
+    m_manX.WhenPressed(m_CollectorSwap);
+    //m_manDPadRight.WhileHeld(Center(&m_driveBase, &m_shooter));
     m_manY.WhenPressed(m_PipelineSwap);
 }
 
